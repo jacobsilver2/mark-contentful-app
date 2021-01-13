26608725f2f1f64c5b85fcc94f241ab3dd13e2ed
Jacob Silver <jacobsilver2@mac.com>
2021-01-13 18:37:34 -0500

    added sort to select

--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -106,7 +106,7 @@ const Field = (props: FieldProps) => {
         onChange={(e) => updateFieldValueSelect(e)}
       >
         {allEntryValues &&
-          allEntryValues.map((v) => (
+          allEntryValues.sort.map((v) => (
             <Option value={v} key={v}>
               {v}
             </Option>
